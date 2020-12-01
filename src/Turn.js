612768b96ecdfe230bb2191c3428d28f81dbdd8e
Cameron Aragon <caragon4695@gmail.com>
2020-11-30 22:12:46 -0700

    Create Turn class

---
+++ src/Turn.js
@@ -0,0 +1,22 @@
+class Turn {
+  constructor(guess, card) {
+    this.guess = guess;
+    this.card = card;
+  }
+
+  returnGuess() {
+    
+  }
+
+  returnCard() {
+
+  }
+
+  evaluateGuess() {
+
+  }
+
+  giveFeedback() {
+
+  }
+}
