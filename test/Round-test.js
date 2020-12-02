c2b7075e6e202c35cd4613bec0085f4e65e064db
Cameron Aragon <caragon4695@gmail.com>
2020-12-02 16:10:21 -0700

    Complete the test file

--- test/Round-test.js
+++ test/Round-test.js
@@ -15,6 +15,50 @@ card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'ga
 card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
 deck = new Deck([card1, card2, card3]);
 round = new Round(deck);
-}
+});
 
-})
+  it.skip('should be a function', function() {
+    expect(Round).to.be.a(function);
+  });
+
+  it.skip('should be an instance of Round', function() {
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it.skip('should store the current deck', function() {
+    expect(round.deck).to.equal(deck);
+  });
+
+  it.skip('should be able to return the current card', function() {
+    expect(round.returnCurrentCard()).to.equal(round.deck[0]);
+  });
+
+  it.skip('should have no turns taken yet', function() {
+    expect(round.turns).to.equal(0);
+  });
+
+  it.skip('should be able to store the incorrect guesses', function() {
+    expect(round.incorrectGuesses).to.equal([]);
+  });
+
+  it.skip('should allow the user to take some turns and return whether it was correct or incorrect', function() {
+    expect(round.takeTurn('sea otter').to.equal('correct!'));
+    expect(round.takeTurn('spleen').to.equal('incorrect!'));
+  });
+
+  it.skip('should have 2 turns taken', function() {
+    expect(round.turns).to.equal(2);
+  });
+
+  it.skip('should add the incorrect guesses\'s id', function() {
+    expect(round.incorrectGuesses).to.deep.equal([14]);
+  });
+
+  it.skip('should be able to return the current card', function() {
+    expect(round.returnCurrentCard()).to.equal(round.deck[1]);
+  });
+
+  it.skip('should calculate and return the percentage of correct guesses', function() {
+    expect(round.calculatePercentCorrect().to.equal(50));
+  });
+});
