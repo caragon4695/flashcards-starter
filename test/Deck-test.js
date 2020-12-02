1a989a9f1b081f9bf77096e8e426a2f8d410cf64
Cameron Aragon <caragon4695@gmail.com>
2020-12-01 22:14:13 -0700

    Build Deck and Deck-test files and test

---
+++ test/Deck-test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+beforeEach((done) => {
+  card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+  card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+  deck = new Deck([card1, card2, card3]);
+  done();
+});
+
+  it('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck', function() {
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it('should be able to count the cards in the deck', function() {
+    expect(deck.countCards(deck)).to.equal(3);
+  })
+});
