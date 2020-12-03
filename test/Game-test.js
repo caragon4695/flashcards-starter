e98b27e494ad491c4eb83b7acb1d62ab2e3be076
Cameron Aragon <caragon4695@gmail.com>
2020-12-02 21:57:27 -0700

    Add mocha and chai to Game-test

---
+++ test/Game-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Card');
+const Round = require('../src/Round');
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Game', function() {
+
+})
