318eb44c70a6b88ffde8ec8b13f99f033e179e0a
Cameron Aragon <caragon4695@gmail.com>
2020-12-03 16:11:51 -0700

    Add to the game test

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,5 +8,24 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Game', function() {
+  let game;
+beforeEach((done) => {
+  game = new Game();
+});
+  it.skip('should be a function', function() {
+    expect(Game).to.be.a('function');
+  });
 
-})
+  it.skip('should be an instance of Game', function() {
+    expect(game).to.be.an.instanceof(Game);
+  });
+
+  it.skip('should keep track of the current round', function() {
+    expect(game.currentRound).to.equal(round)
+  })
+
+  it.skip('should start the flash cards game', function() {
+    game.start()
+    expect(game.start()).to.eqaul()
+  });
+});
